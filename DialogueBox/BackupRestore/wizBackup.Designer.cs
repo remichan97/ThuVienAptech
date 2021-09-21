c52ba6597d8bdc2a8061b0c654a57691f83c15f9
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2021-09-21 23:00:40 +0700

    Feature: Hoàn thành chức năng sao lưu

---
+++ DialogueBox/BackupRestore/wizBackup.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ThuVien.DialogueBox.BackupRestore
+{
+    partial class wizBackup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(wizBackup));
+            this.wizardControl1 = new AeroWizard.WizardControl();
+            this.landingPage = new AeroWizard.WizardPage();
+            this.backupPage = new AeroWizard.WizardPage();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowseFolder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.wizardControl1)).BeginInit();
+            this.landingPage.SuspendLayout();
+            this.backupPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // wizardControl1
+            // 
+            this.wizardControl1.BackButtonToolTipText = "Quay trở lại bước trước đó";
+            this.wizardControl1.BackColor = System.Drawing.Color.White;
+            this.wizardControl1.CancelButtonText = "Thoát";
+            this.wizardControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wizardControl1.FinishButtonText = "Sao lưu";
+            this.wizardControl1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wizardControl1.Location = new System.Drawing.Point(0, 0);
+            this.wizardControl1.Name = "wizardControl1";
+            this.wizardControl1.NextButtonText = "Tiếp tục";
+            this.wizardControl1.Pages.Add(this.landingPage);
+            this.wizardControl1.Pages.Add(this.backupPage);
+            this.wizardControl1.Size = new System.Drawing.Size(619, 408);
+            this.wizardControl1.TabIndex = 0;
+            this.wizardControl1.Text = "Wizard Title";
+            this.wizardControl1.Title = "Sao lưu dữ liệu";
+            this.wizardControl1.TitleIcon = ((System.Drawing.Icon)(resources.GetObject("wizardControl1.TitleIcon")));
+            // 
+            // landingPage
+            // 
+            this.landingPage.Controls.Add(this.btnBrowseFolder);
+            this.landingPage.Controls.Add(this.txtFolder);
+            this.landingPage.Controls.Add(this.label1);
+            this.landingPage.Name = "landingPage";
+            this.landingPage.Size = new System.Drawing.Size(572, 254);
+            this.landingPage.TabIndex = 0;
+            this.landingPage.Text = "Vui lòng chọn đường dẫn sao lưu dữ liệu";
+            this.landingPage.Commit += new System.EventHandler<AeroWizard.WizardPageConfirmEventArgs>(this.landingPage_Commit);
+            // 
+            // backupPage
+            // 
+            this.backupPage.Controls.Add(this.label2);
+            this.backupPage.Name = "backupPage";
+            this.backupPage.Size = new System.Drawing.Size(572, 254);
+            this.backupPage.TabIndex = 1;
+            this.backupPage.Text = "Sẵn sàng sao lưu dữ liệu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 105);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Enabled = false;
+            this.txtFolder.Location = new System.Drawing.Point(3, 159);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(446, 23);
+            this.txtFolder.TabIndex = 1;
+            // 
+            // btnBrowseFolder
+            // 
+            this.btnBrowseFolder.Location = new System.Drawing.Point(467, 158);
+            this.btnBrowseFolder.Name = "btnBrowseFolder";
+            this.btnBrowseFolder.Size = new System.Drawing.Size(102, 23);
+            this.btnBrowseFolder.TabIndex = 2;
+            this.btnBrowseFolder.Text = "Chọn thư mục";
+            this.btnBrowseFolder.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nhấn nút kết thúc để bắt đầu quá trình sao lưu";
+            // 
+            // wizBackup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 408);
+            this.Controls.Add(this.wizardControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "wizBackup";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            ((System.ComponentModel.ISupportInitialize)(this.wizardControl1)).EndInit();
+            this.landingPage.ResumeLayout(false);
+            this.landingPage.PerformLayout();
+            this.backupPage.ResumeLayout(false);
+            this.backupPage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AeroWizard.WizardControl wizardControl1;
+        private AeroWizard.WizardPage landingPage;
+        private AeroWizard.WizardPage backupPage;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ DialogueBox/BackupRestore/wizBackup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AeroWizard;
+
+namespace ThuVien.DialogueBox.BackupRestore
+{
+    public partial class wizBackup : Form
+    {
+        public wizBackup()
+        {
+            InitializeComponent();
+        }
+
+        private void landingPage_Commit(object sender, AeroWizard.WizardPageConfirmEventArgs e)
+        {
+            
+        }
+    }
+}
